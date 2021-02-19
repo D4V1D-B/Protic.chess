65b7ecf86d78040911f8386bfa20f7fa18f5f664
Marc-AnthonyG <78452236+Marc-AnthonyG@users.noreply.github.com>
2021-02-19 12:01:48 -0500

    Update Cavalier.java

--- src/Modele/Cavalier.java
+++ src/Modele/Cavalier.java
@@ -1,14 +1,54 @@
 package Modele;
 
 import java.awt.Point;
+import java.util.ArrayList;
 
 public class Cavalier extends Pieces
 {
+	private ArrayList<Point> mouvementPossible;
 
 	public Cavalier(String nom, boolean couleur, Point position)
 	{
 		super(nom, couleur, position);
-		// TODO Auto-generated constructor stub
 	}
 
+	public ArrayList<Point> getMouvementPossible()
+	{
+		return mouvementPossible;
+	}
+
+	public void setMouvementPossible(Object[][] plateau,
+			ArrayList<Point> positionEnemie)
+	{
+		ArrayList<Point> variationMouvement = new ArrayList<Point>();
+		variationMouvement.add(new Point(-1, -2));
+		variationMouvement.add(new Point(-2, -1));
+		variationMouvement.add(new Point(-2, 1));
+		variationMouvement.add(new Point(-1, 2));
+		variationMouvement.add(new Point(1, 2));
+		variationMouvement.add(new Point(2, 1));
+		variationMouvement.add(new Point(2, -1));
+		variationMouvement.add(new Point(1, -2));
+		mouvementPossible.clear();
+
+		int i = this.getEmplacement().x;
+		int j = this.getEmplacement().y;
+
+		for (Point a : variationMouvement)
+		{
+			if (plateau[(int) (i + a.getX())][(int) (j + a.getY())] == null)
+			{
+				mouvementPossible.add(new Point((int) (i + a.getX()), (int) (j + a.getY())));
+			}
+			else
+			{
+				for (Point x : positionEnemie)
+				{
+					if (x.equals(new Point((int) (i + a.getX()), (int) (j + a.getY()))))
+						mouvementPossible.add(x);
+				}
+			}
+		}
+
+	}
 }
