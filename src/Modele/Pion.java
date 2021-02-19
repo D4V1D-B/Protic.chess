be4921298c94f00b2fd56bfc8f61689e1ac32f2c
DavidBernier59 <david16bernier@hotmail.ca>
2021-02-19 10:39:57 -0500

    modification de pièces et pion

--- src/Modele/Pion.java
+++ src/Modele/Pion.java
@@ -5,13 +5,28 @@ import java.util.ArrayList;
 
 public class Pion extends Pieces {
 
-	private boolean aBouger = false;
-	private ArrayList<String> mouvementPossible;
-	
-	
-	public Pion(String nom, boolean couleur, Point emplacement){
+	private boolean aBouger;
+
+	private ArrayList<Point> mouvementPossible;
+
+	public Pion(String nom, boolean couleur, Point emplacement) {
 		super(nom, couleur, emplacement);
-		// TODO Auto-generated constructor stub
+		setaBouger(false);
 	}
 
+	public boolean isaBouger() {
+		return aBouger;
+	}
+
+	public void setaBouger(boolean aBouger) {
+		this.aBouger = aBouger;
+	}
+
+	public ArrayList<Point> getMouvementPossible() {
+		return mouvementPossible;
+	}
+
+	public void setMouvementPossible(ArrayList<Point> mouvementPossible) {
+		this.mouvementPossible = mouvementPossible;
+	}
 }
