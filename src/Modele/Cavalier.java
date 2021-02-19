d9992918a42f390f2a75b542423e8e26cde83dec
DavidBernier59 <david16bernier@hotmail.ca>
2021-02-19 10:00:59 -0500

    ajout des pieces

--- src/Modele/Cavalier.java
+++ src/Modele/Cavalier.java
@@ -1,8 +1,8 @@
 package Modele;
 
-public class Reine extends Pieces {
+public class Cavalier extends Pieces {
 
-	public Reine(String nom, boolean couleur) {
+	public Cavalier(String nom, boolean couleur) {
 		super(nom, couleur);
 		// TODO Auto-generated constructor stub
 	}
