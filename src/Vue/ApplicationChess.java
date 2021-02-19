2a8f9461a8911708b37ac7a82cfec3b6e2325321
DavidBernier59 <david16bernier@hotmail.ca>
2021-02-19 09:54:53 -0500

    Sam application

---
+++ src/Vue/ApplicationChess.java
@@ -0,0 +1,28 @@
+package Vue;
+
+import Controleur.Controleur;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ApplicationChess extends Application {
+
+	public static void main(String[] args) {
+	ApplicationChess.launch();
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/res/baord.fxml"));
+		primaryStage.setScene(new Scene(loader.load()));
+
+		Controleur controleur = loader.getController();
+		primaryStage.setTitle("Échec");
+		primaryStage.show();
+		
+	}
+
+}
