b33dd61f27525e7c864fa0eeb58d76db80cce6b7
Sam17-beep <78397416+Sam17-beep@users.noreply.github.com>
2021-03-10 09:20:24 -0500

    test
    Truc random, jarrive pas a faire des test :(

--- src/test/modele/CavalierTest.java
+++ src/test/modele/CavalierTest.java
@@ -9,7 +9,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import controleur.Plateau;
+import controleur.Plateau.Equipe;
 import modele.Cavalier;
+import modele.Pieces;
 
 public class CavalierTest
 {
@@ -25,30 +27,39 @@ public class CavalierTest
 	@Before
 	public void testCavalier()
 	{
-//		// plateau vide
 
-//		// plateau vide
-
-
+	
+	
 		// cavalier sur le bord
-		cavalierB = new Cavalier("cavalierB", true, new Point(0, 4));
-
+		cavalierB = new Cavalier("cavalierB", true, new Point(4, 0));
+		
 		// cavalier au centre
 		cavalierC = new Cavalier("cavalierC", true, new Point(4, 4));
 
 		// cavalier dans un coin
 		cavalierc = new Cavalier("cavalierc", true, new Point(7, 7));
+
+	
 	}
+
 	/**
 	 * Pas besoin de test d'invalidité, car les inputs seront toujours bon en
 	 * théorie. Si,il y a une erreur elle proviendra d'une autre partie du code.
 	 */
-	
+
 	@Test
 	public void testSetMouvementPossible()
 	{
-		//cavalier du bord peut aller sur  1,6  / 2,5  / 2,3 / 1,2
-		
-		
+		// cavalier du bord peut aller sur 1,6 / 2,5 / 2,3 / 1,2
+
+		assertEquals(true,
+				cavalierB.getMouvementPossible().contains(new Point(2, 1)));
+		assertEquals(true,
+				cavalierB.getMouvementPossible().contains(new Point(3, 2)));
+		assertEquals(true,
+				cavalierB.getMouvementPossible().contains(new Point(5, 2)));
+		assertEquals(true,
+				cavalierB.getMouvementPossible().contains(new Point(6, 1)));
+
 	}
 }
