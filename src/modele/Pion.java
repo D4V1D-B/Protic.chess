fb8e24407691ba4d057c3508117c272e17067688
Marc-Anthony <78452236+Marc-AnthonyG@users.noreply.github.com>
2021-03-10 20:46:39 -0500

    pu personne touche au pion

--- src/modele/Pion.java
+++ src/modele/Pion.java
@@ -30,7 +30,7 @@ public class Pion extends Pieces
 		if (this.isWhite())
 		{
 			// Premier mouvement 2 case
-			if ((this.getEmplacement().getY()==1)
+			if (j == 1
 					&& OperationSurUneMatrice.getVoidSpace(plateau)
 							.contains(new Point(i, j + 1))
 					&& OperationSurUneMatrice.getVoidSpace(plateau)
@@ -47,45 +47,28 @@ public class Pion extends Pieces
 			}
 
 			// droite case diagonal
-<<<<<<< HEAD
-<<<<<<< HEAD
-			if (i+1<=7 &&j-1<=0 &&!OperationSurUneMatrice.getVoidSpace(plateau)
-					.contains(new Point(i - 1, j + 1)) && i - 1 >= 0 && !plateau[i+1][j-1]
-=======
-			if (!OperationSurUneMatrice.getVoidSpace(plateau)
-					.contains(new Point(i - 1, j + 1)) && i - 1 >= 0 && !plateau[this.getEmplacement().x][j]
->>>>>>> parent of 77eba46 (test pion)
-							.isWhite())
-=======
-			if (!OperationSurUneMatrice.getVoidSpace(plateau)
-					.contains(new Point(i - 1, j + 1)) && i - 1 >= 0)
->>>>>>> parent of 0c19781 (updapte pion mangeur de pion)
+			if (i + 1 <= 7 && j + 1 <= 7
+					&& !OperationSurUneMatrice.getVoidSpace(plateau)
+							.contains(new Point(i + 1, j + 1))
+					&& !plateau[i + 1][j + 1].isWhite())
 			{
-				this.getMouvementPossible().add(new Point(i - 1, j + 1));
+				this.getMouvementPossible().add(new Point(i + 1, j + 1));
 			}
 
 			// gauche case diagonal
-<<<<<<< HEAD
-<<<<<<< HEAD
-			if (i+1<=7 &&j+1<=7 && !OperationSurUneMatrice.getVoidSpace(plateau)
-=======
-			if (!OperationSurUneMatrice.getVoidSpace(plateau)
->>>>>>> parent of 77eba46 (test pion)
-					.contains(new Point(i + 1, j + 1)) && i + 1 <= 7 && !plateau[i+1][j+1]
-							.isWhite())
-=======
-			if (!OperationSurUneMatrice.getVoidSpace(plateau)
-					.contains(new Point(i + 1, j + 1)) && i + 1 <= 7)
->>>>>>> parent of 0c19781 (updapte pion mangeur de pion)
+			if (i - 1 >= 0 && j + 1 <= 7
+					&& !OperationSurUneMatrice.getVoidSpace(plateau)
+							.contains(new Point(i - 1, j + 1))
+					&& !plateau[i - 1][j + 1].isWhite())
 			{
-				this.getMouvementPossible().add(new Point(i + 1, j + 1));
+				this.getMouvementPossible().add(new Point(i - 1, j + 1));
 			}
 
 		}
 		else
 		{
 			// Premier mouvement 2 case
-			if (this.getEmplacement().getY()==6
+			if (this.getEmplacement().getY() == 6
 					&& OperationSurUneMatrice.getVoidSpace(plateau)
 							.contains(new Point(i, j - 1))
 					&& OperationSurUneMatrice.getVoidSpace(plateau)
@@ -102,39 +85,22 @@ public class Pion extends Pieces
 			}
 
 			// droite case diagonal
-<<<<<<< HEAD
-<<<<<<< HEAD
-			if (i - 1 >= 0 && j - 1 >=0 && !OperationSurUneMatrice.getVoidSpace(plateau)
-					.contains(new Point(i - 1, j - 1)) && plateau[i - 1][j-1]
-=======
-			if (!OperationSurUneMatrice.getVoidSpace(plateau)
-					.contains(new Point(i - 1, j - 1)) && i - 1 >= 0&& plateau[i - 1][j-1]
->>>>>>> parent of 77eba46 (test pion)
-							.isWhite())
-=======
-			if (!OperationSurUneMatrice.getVoidSpace(plateau)
-					.contains(new Point(i - 1, j - 1)) && i - 1 >= 0)
->>>>>>> parent of 0c19781 (updapte pion mangeur de pion)
+
+			if (i + 1 <= 7 && j - 1 >= 0
+					&& !OperationSurUneMatrice.getVoidSpace(plateau)
+							.contains(new Point(i + 1, j - 1))
+					&& plateau[i + 1][j - 1].isWhite())
 			{
-				this.getMouvementPossible().add(new Point(i - 1, j - 1));
+				this.getMouvementPossible().add(new Point(i + 1, j - 1));
 			}
 
 			// gauche case diagonal
-<<<<<<< HEAD
-<<<<<<< HEAD
-			if (i - 1 >= 0 && j + 1 <=7 && !OperationSurUneMatrice.getVoidSpace(plateau )
-					.contains(new Point(i + 1, j - 1)) && i + 1 <= 7&& plateau[i - 1][j+1]
-=======
-			if (!OperationSurUneMatrice.getVoidSpace(plateau )
-					.contains(new Point(i + 1, j - 1)) && i + 1 <= 7&& plateau[this.getEmplacement().x][j]
->>>>>>> parent of 77eba46 (test pion)
-							.isWhite())
-=======
-			if (!OperationSurUneMatrice.getVoidSpace(plateau)
-					.contains(new Point(i + 1, j - 1)) && i + 1 <= 7)
->>>>>>> parent of 0c19781 (updapte pion mangeur de pion)
+			if (i - 1 >= 0 && j - 1 >= 0
+					&& !OperationSurUneMatrice.getVoidSpace(plateau)
+							.contains(new Point(i - 1, j - 1))
+					&& plateau[i-1][j-1].isWhite())
 			{
-				this.getMouvementPossible().add(new Point(i + 1, j - 1));
+				this.getMouvementPossible().add(new Point(i - 1, j - 1));
 			}
 		}
 
