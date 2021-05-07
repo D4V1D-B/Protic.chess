c7a077ecd5efa79ca622340607c0527267dfeaba
DavidBernier59 <david16bernier@hotmail.ca>
2021-05-07 09:50:01 -0400

    alpha beta

--- src/controleur/Bot.java
+++ src/controleur/Bot.java
@@ -14,7 +14,13 @@ public class Bot
 	public Move jouerBot(Plateau plateau)
 	{
 		this.plateau = plateau; // prend le plateau
-		return MinMax(3).getValue(); // retourne le déplacement pour l'image seulement
+		return alphaBeta(3, (int) Double.NEGATIVE_INFINITY, (int) Double.POSITIVE_INFINITY).getValue();
+		// retourne
+		// le
+		// déplacement
+		// pour
+		// l'image
+		// seulement
 
 	}
 
@@ -22,11 +28,11 @@ public class Bot
 	{
 		if (!plateau.getNoir().getMouvementJouable().equals(null))
 		{
-			return rechercheMax(3);
+			return rechercheMax(depth);
 		}
 		else
 		{
-			return rechercheMin(3);
+			return rechercheMin(depth);
 		}
 
 	}
@@ -71,14 +77,14 @@ public class Bot
 																	// meilleurs
 																	// des
 																	// blanc.
-			if (evaluation> bestMovement.getKey()) // vérifie si le movement
+			if (evaluation > bestMovement.getKey()) // vérifie si le movement
 													// actuelle c'est le
 													// meilleur
 			{
 				bestMovement = new Pair<Integer, Move>(evaluation, mov);
 			}
 
-			plateau.unMakeMove(bestMovement.getValue().getPoint(), piece, reverseMove );
+			plateau.unMakeMove(bestMovement.getValue().getPoint(), piece, reverseMove);
 			plateau.actualiserTeam();
 
 		}
@@ -135,8 +141,69 @@ public class Bot
 				bestMovement = new Pair<Integer, Move>(evaluation, mov);
 			}
 
-			
 			plateau.unMakeMove(bestMovement.getValue().getPoint(), piece, reverseMove);
+
+			plateau.actualiserTeam();
+
+		}
+
+		return bestMovement;
+
+	}
+
+	private Pair<Integer, Move> alphaBeta(int depth, int alpha, int beta)
+	{
+
+		Pair<Integer, Move> bestMovement;
+		if (depth <= 0)
+		{
+			return new Pair<Integer, Move>(evaluerEquipe(), null);
+		}
+
+		ArrayList<Move> movePossible = generationMove(); // mouvement jouable
+		if (movePossible.size() == 0) // regarde si la liste est pas vide
+		{
+			return new Pair<Integer, Move>((int) Double.POSITIVE_INFINITY, null);
+		}
+
+		bestMovement = new Pair<Integer, Move>((int) Double.POSITIVE_INFINITY, null); // instancier
+																						// best
+																						// move
+		for (Move mov : movePossible) // pour chaque move
+		{
+			Pieces piece = mov.getPieces(); // prend 1 pieces
+			Point anciennePosition = piece.getEmplacement(); // ancienne
+																// emplacement
+
+			Pieces reverseMove = deplacement(anciennePosition, piece); // ancien
+																		// emplacement
+																		// /
+																		// pieces
+																		// pour
+																		// faire
+																		// le
+																		// déplacememnt
+			int evaluation = (alphaBeta(depth - 1, -beta, -alpha)).getKey(); // prend
+																				// le
+			// meilleur
+			// move des
+			// noirs et
+			// regarde
+			// le
+			// meilleurs
+			// des
+			// blanc.
+			plateau.unMakeMove(bestMovement.getValue().getPoint(), piece, reverseMove);
+
+			if (evaluation >= beta)
+			{
+				return new Pair<Integer, Move>(beta, mov);
+			}
+
+			if (evaluation > alpha)
+			{
+				bestMovement = new Pair<Integer, Move>(alpha, mov);
+			}
 
 			plateau.actualiserTeam();
 
@@ -161,7 +228,6 @@ public class Bot
 			plateau.ajouterEnPassant(anciennePosition, piecesDeplacer);
 		}
 
-		
 		return manger;
 	}
 
